fcb192ae76c7d99c42a7eb863457eeeef20c1e59
carlfeynman <coolarun477@gmail.com>
2026-03-22 11:05:09 +0530

    Auto commit

--- envs/my_echo_env/test_client.py
+++ envs/my_echo_env/test_client.py
@@ -1,11 +1,11 @@
-from my_echo_env import MyEchoEnv, MyEchoAction
+from my_echo_env import MyEchoEnv, CounterAction
 
 
 env = MyEchoEnv(base_url="http://localhost:8000").sync()
 with env:
     result = env.reset()
     print("Reset:", result.observation.echoed_message)
-    result = env.step(MyEchoAction(message="Hello!"))
+    result = env.step(CounterAction(action="increment"))
     print("Echo:", result.observation.echoed_message)
     print("Length:", result.observation.message_length)
     print("Reward:", result.reward)
